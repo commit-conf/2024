9408b37df7343cb3c4f35c3662dce638ae9053f3
Carlos Coloma <ccescribano@gmail.com>
2023-09-07 15:34:26 +0100

    First commit

---
+++ src/_data/communities.js
@@ -0,0 +1,228 @@
+module.exports = [
+  {
+    name: "Adalab",
+    href: "http://adalab.es/",
+  },
+  {
+    name: "Agile Aragon",
+    href: "https://agile-aragon.org/",
+  },
+  {
+    name: "AsturiasHacking",
+    href: "https://www.meetup.com/AsturiasHacking/",
+  },
+  {
+    name: "Betabeers Madrid",
+    href: "http://betabeers.com",
+  },
+  {
+    name: "Big Girls Theory",
+    href: "https://biggirlstheory.es/",
+  },
+  {
+    name: "CanariasJS",
+    href: "https://jsdaycanarias.com/",
+    svg: "true",
+  },
+  {
+    name: "CATzure",
+    href: "https://www.meetup.com/es-ES/CATzure/",
+  },
+  {
+    name: "CrossDevelopment-Madrid",
+    href: "http://www.xdevmadrid.es/",
+  },
+  {
+    name: "CrossDvlup Madrid",
+    href: "https://www.meetup.com/CrossDvlup",
+  },
+  {
+    name: "Docker Madrid",
+    href: "https://www.meetup.com/es-ES/Docker-Madrid/",
+  },
+  {
+    name: "dotnetMalaga",
+    href: "http://dotnetmalaga.es/",
+  },
+  {
+    name: "DotNetters",
+    href: "https://www.meetup.com/es-ES/dotnetters",
+  },
+  {
+    name: "Facebook Developer Circle Madrid",
+    href: "https://www.facebook.com/groups/DevCMadrid",
+    svg: "true",
+  },
+  {
+    name: "FemDevs",
+    href: "https://femdevs.es/",
+  },
+  {
+    name: "Frontend Developers Madrid",
+    href: "https://www.meetup.com/es-ES/Front-end-Developers-Madrid/",
+  },
+  {
+    name: "GDG`s",
+    href: "http://www.gdg.es",
+  },
+  {
+    name: "GDG Toledo",
+    href: "https://gdg.community.dev/gdg-toledo/",
+  },
+  {
+    name: "GeoDevelopers",
+    href: "http://meetup.com/Geo-Developers/",
+  },
+  {
+    name: "Girls in Tech Spain",
+    href: "https://spain.girlsintech.org/",
+  },
+  {
+    name: "HackathonLovers",
+    href: "http://hackathonlovers.com/",
+  },
+  {
+    name: "HackMadrid %27",
+    href: "https://www.meetup.com/HackMadrid-27/",
+  },
+  {
+    name: "HTML5Spain",
+    href: "https://www.meetup.com/es-ES/HTML5-Spain/",
+  },
+  {
+    name: "Ionic Madrid",
+    href: "https://www.meetup.com/Ionic-Madrid/",
+    svg: "true",
+  },
+  {
+    name: "javaHispano",
+    href: "http://www.javahispano.org/",
+  },
+  {
+    name: "Madriagil",
+    href: "https://www.meetup.com/es-ES/madriagil/",
+  },
+  {
+    name: "Madrid Android Developer Group",
+    href: "https://www.meetup.com/madrid-android-dev-group/",
+  },
+  {
+    name: "Madrid Spring User Group",
+    href: "https://www.meetup.com/madrid-spring-user-group/",
+  },
+  {
+    name: "Madrid SUG",
+    href: "https://www.meetup.com/MadridSUG",
+  },
+  {
+    name: "Madrid |> Elixir",
+    href: "https://www.meetup.com/Madrid-Elixir/",
+  },
+  {
+    name: "MadridDotNet",
+    href: "https://www.meetup.com/madriddotnet/",
+  },
+  {
+    name: "MadridJS",
+    href: "https://www.meetup.com/es-ES/madridjs/",
+  },
+  {
+    name: "MadridJUG",
+    href: "https://www.meetup.com/es-ES/MadridJUG/",
+  },
+  {
+    name: "MadridScala",
+    href: "https://www.meetup.com/Scala-Programming-Madrid/",
+  },
+  {
+    name: "madrid.rb",
+    href: "http://madridrb.com",
+  },
+  {
+    name: "Madrid-GUG",
+    href: "http://www.madridgug.com/",
+  },
+  {
+    name: "MadRust",
+    href: "https://www.meetup.com/MadRust/",
+  },
+  {
+    name: "Málaga Scala Developers",
+    href: "https://www.meetup.com/es-ES/Malaga-Scala/",
+  },
+  {
+    name: "MálagaJUG",
+    href: "https://meetup.com/es-ES/MalagaJUG/",
+  },
+  {
+    name: "MsCoders Madrid",
+    href: "https://www.meetup.com/MSCoders/",
+  },
+  {
+    name: "My Tech Plan",
+    href: "https://www.mytechplan.com/",
+  },
+  {
+    name: "NodeJSMadrid",
+    href: "https://www.meetup.com/es/Node-js-Madrid",
+  },
+  {
+    name: "NSCoders Night Madrid",
+    href: "https://www.meetup.com/es-ES/NSCoder-Night-Madrid/",
+  },
+  {
+    name: "PASS Spain",
+    href: "http://www.sqlpass.es",
+  },
+  {
+    name: "PhpMadrid",
+    href: "https://www.meetup.com/phpmad/",
+  },
+  {
+    name: "Python España",
+    href: "https://es.python.org/",
+    svg: "true",
+  },
+  {
+    name: "Python Madrid",
+    href: "https://www.meetup.com/python-madrid/",
+    svg: "true",
+  },
+  {
+    name: "PyData Madrid",
+    href: "https://meetup.com/PyData-Madrid/",
+    svg: "true",
+  },
+  {
+    name: "Rioja{dotnet}",
+    href: "http://www.riojadotnet.com/",
+  },
+  {
+    name: "Software Crafters Málaga",
+    href: "https://www.meetup.com/swcraftmlg/",
+  },
+  {
+    name: "STCAsturias",
+    href: "https://twitter.com/STCAsturias",
+  },
+  {
+    name: "SUG.CAT",
+    href: "http://www.sug.cat",
+  },
+  {
+    name: "Techdencias",
+    href: "http://www.techdencias.net/",
+  },
+  {
+    name: "Tech&Ladies",
+    href: "http://techandladies.com",
+  },
+  {
+    name: "VueJS Madrid",
+    href: "https://www.meetup.com/VueJS-Madrid/",
+  },
+  {
+    name: "XantarDev",
+    href: "https://xantardev.org",
+  },
+];
---
+++ src/_data/site.js
@@ -0,0 +1,20 @@
+const communities = require("./communities.js");
+const sponsors = require("./sponsors.js");
+
+module.exports = function () {
+  return {
+    buildTime: new Date(),
+    baseUrl: "https://2024.commit-conf.com",
+    name: "Commit Conf",
+    email: "info@commit-conf.com",
+    twitter: "@commitconf",
+    youtubeChannel: "UCd_1KHg4t2VKGsSDF8OD5Cw",
+    environment: process.env.ENV || "prod",
+    c4pURL: "https://koliseo.com/events/commit-2024/agenda",
+    ticketsURL: "https://koliseo.com/events/commit-2024/tickets",
+    newsletter:
+      "https://commit-conf.us8.list-manage.com/subscribe?u=b180f87ed019c243f111b693a&amp;id=6a6bdcb448",
+    communities,
+    sponsors,
+  };
+};
