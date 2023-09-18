fd4c55e0cc7c173414e55d29c905a8c30ebcfecf
Carlos Coloma <ccescribano@gmail.com>
2023-09-18 14:12:52 +0100

    Update Koliseo links

--- src/_data/site.js
+++ src/_data/site.js
@@ -10,8 +10,9 @@ module.exports = function () {
     twitter: "@commitconf",
     youtubeChannel: "UCd_1KHg4t2VKGsSDF8OD5Cw",
     environment: process.env.ENV || "prod",
-    c4pURL: "https://koliseo.com/events/commit-2024/agenda",
-    ticketsURL: "https://koliseo.com/events/commit-2024/tickets",
+    c4pURL: "https://koliseo.com/commit/2024/sessions",
+    agendaURL: "https://koliseo.com/commit/2024/agenda",
+    ticketsURL: "https://koliseo.com/commit/2024/tickets",
     newsletter:
       "https://commit-conf.us8.list-manage.com/subscribe?u=b180f87ed019c243f111b693a&amp;id=6a6bdcb448",
     communities,
