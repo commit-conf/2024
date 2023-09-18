b5a9b0fda381aaaf8a4e445bd7c30d47239e0db4
Carlos Coloma <ccescribano@gmail.com>
2023-09-18 14:11:53 +0100

    Update communities

--- src/_data/communities.js
+++ src/_data/communities.js
@@ -1,61 +1,15 @@
 module.exports = [
   {
-    name: "Adalab",
-    href: "http://adalab.es/",
-  },
-  {
     name: "Agile Aragon",
     href: "https://agile-aragon.org/",
   },
   {
-    name: "AsturiasHacking",
-    href: "https://www.meetup.com/AsturiasHacking/",
-  },
-  {
-    name: "Betabeers Madrid",
-    href: "http://betabeers.com",
-  },
-  {
-    name: "Big Girls Theory",
-    href: "https://biggirlstheory.es/",
-  },
-  {
-    name: "CanariasJS",
-    href: "https://jsdaycanarias.com/",
-    svg: "true",
-  },
-  {
-    name: "CATzure",
-    href: "https://www.meetup.com/es-ES/CATzure/",
-  },
-  {
-    name: "CrossDevelopment-Madrid",
-    href: "http://www.xdevmadrid.es/",
+    name: "AgileGirls",
+    href: "https://twitter.com/agilegirls",
   },
   {
     name: "CrossDvlup Madrid",
     href: "https://www.meetup.com/CrossDvlup",
-  },
-  {
-    name: "Docker Madrid",
-    href: "https://www.meetup.com/es-ES/Docker-Madrid/",
-  },
-  {
-    name: "dotnetMalaga",
-    href: "http://dotnetmalaga.es/",
-  },
-  {
-    name: "DotNetters",
-    href: "https://www.meetup.com/es-ES/dotnetters",
-  },
-  {
-    name: "Facebook Developer Circle Madrid",
-    href: "https://www.facebook.com/groups/DevCMadrid",
-    svg: "true",
-  },
-  {
-    name: "FemDevs",
-    href: "https://femdevs.es/",
   },
   {
     name: "Frontend Developers Madrid",
@@ -66,37 +20,16 @@ module.exports = [
     href: "http://www.gdg.es",
   },
   {
-    name: "GDG Toledo",
-    href: "https://gdg.community.dev/gdg-toledo/",
-  },
-  {
     name: "GeoDevelopers",
     href: "http://meetup.com/Geo-Developers/",
-  },
-  {
-    name: "Girls in Tech Spain",
-    href: "https://spain.girlsintech.org/",
   },
   {
     name: "HackathonLovers",
     href: "http://hackathonlovers.com/",
   },
   {
-    name: "HackMadrid %27",
-    href: "https://www.meetup.com/HackMadrid-27/",
-  },
-  {
     name: "HTML5Spain",
     href: "https://www.meetup.com/es-ES/HTML5-Spain/",
-  },
-  {
-    name: "Ionic Madrid",
-    href: "https://www.meetup.com/Ionic-Madrid/",
-    svg: "true",
-  },
-  {
-    name: "javaHispano",
-    href: "http://www.javahispano.org/",
   },
   {
     name: "Madriagil",
@@ -109,14 +42,6 @@ module.exports = [
   {
     name: "Madrid Spring User Group",
     href: "https://www.meetup.com/madrid-spring-user-group/",
-  },
-  {
-    name: "Madrid SUG",
-    href: "https://www.meetup.com/MadridSUG",
-  },
-  {
-    name: "Madrid |> Elixir",
-    href: "https://www.meetup.com/Madrid-Elixir/",
   },
   {
     name: "MadridDotNet",
@@ -135,16 +60,8 @@ module.exports = [
     href: "https://www.meetup.com/Scala-Programming-Madrid/",
   },
   {
-    name: "madrid.rb",
-    href: "http://madridrb.com",
-  },
-  {
     name: "Madrid-GUG",
     href: "http://www.madridgug.com/",
-  },
-  {
-    name: "MadRust",
-    href: "https://www.meetup.com/MadRust/",
   },
   {
     name: "Málaga Scala Developers",
@@ -155,28 +72,8 @@ module.exports = [
     href: "https://meetup.com/es-ES/MalagaJUG/",
   },
   {
-    name: "MsCoders Madrid",
-    href: "https://www.meetup.com/MSCoders/",
-  },
-  {
-    name: "My Tech Plan",
-    href: "https://www.mytechplan.com/",
-  },
-  {
     name: "NodeJSMadrid",
     href: "https://www.meetup.com/es/Node-js-Madrid",
-  },
-  {
-    name: "NSCoders Night Madrid",
-    href: "https://www.meetup.com/es-ES/NSCoder-Night-Madrid/",
-  },
-  {
-    name: "PASS Spain",
-    href: "http://www.sqlpass.es",
-  },
-  {
-    name: "PhpMadrid",
-    href: "https://www.meetup.com/phpmad/",
   },
   {
     name: "Python España",
@@ -194,32 +91,8 @@ module.exports = [
     svg: "true",
   },
   {
-    name: "Rioja{dotnet}",
-    href: "http://www.riojadotnet.com/",
-  },
-  {
-    name: "Software Crafters Málaga",
-    href: "https://www.meetup.com/swcraftmlg/",
-  },
-  {
     name: "STCAsturias",
     href: "https://twitter.com/STCAsturias",
-  },
-  {
-    name: "SUG.CAT",
-    href: "http://www.sug.cat",
-  },
-  {
-    name: "Techdencias",
-    href: "http://www.techdencias.net/",
-  },
-  {
-    name: "Tech&Ladies",
-    href: "http://techandladies.com",
-  },
-  {
-    name: "VueJS Madrid",
-    href: "https://www.meetup.com/VueJS-Madrid/",
   },
   {
     name: "XantarDev",
